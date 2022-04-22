b35917d64522edc55fb32f15f655be0c4675625d
pratikfuse <pratik.manandhar@fusemachines.com>
2022-04-22 22:36:02 +0545

    adds base url to configs

--- web-app/src/config/index.ts
+++ web-app/src/config/index.ts
@@ -1,9 +1,5 @@
+const configs = {
+  baseUrl: process.env.REACT_APP_BASE_URL,
+};
 
-export default {
-    github: {
-        clientId: process.env.REACT_APP_CLIENT_ID,
-        clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-        redirectUri: process.env.REACT_APP_REDIRECT_URI
-    },
-    baseUrl: process.env.REACT_APP_BASE_URL
-}
+export default configs;
